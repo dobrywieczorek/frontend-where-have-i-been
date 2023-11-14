445a06f14c1dca620701318b52b34a7399d76ae3
Wiktor <109503651+Wiktor-Wojciechowski@users.noreply.github.com>
2023-11-14 15:20:58 +0100

    copy

---
+++ react-whib/src/App.js
@@ -0,0 +1,13 @@
+import './css/App.css';
+import './components/UserProfile';
+import UserProfile from './components/UserProfile';
+
+function App() {
+  return (
+    <div className="App">
+      <UserProfile />
+    </div>
+  );
+}
+
+export default App;
