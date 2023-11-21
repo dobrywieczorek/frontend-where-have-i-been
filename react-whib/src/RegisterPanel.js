2408636ec622ab3dda04384205f9b509b017709f
W-Worach <wojciech.worach@studenci.collegiumwitelona.pl>
2023-11-21 16:22:42 +0100

    RegisterPage

---
+++ react-whib/src/RegisterPanel.js
@@ -0,0 +1,88 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './Register.css';
+
+const RegisterPanel = () => {
+    //const history = useHistory();
+  
+    const [formData, setFormData] = useState({
+      username: '',
+      email: '',
+      password: '',
+    });
+  
+    const handleChange = (e) => {
+      const { name, value } = e.target;
+      setFormData((prevData) => ({
+        ...prevData,
+        [name]: value,
+      }));
+    };
+  
+    const handleSubmit = async (e) => {
+      e.preventDefault();
+
+
+  /*
+      try {
+        const { ...dataToSend } = formData; // Exclude confirmPassword from data to send
+  
+        const response = await fetch('http://localhost:8000/api/register', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(dataToSend) // Send only necessary data to the backend
+        });
+  
+        if (response.ok) {
+          //history.push('/login');
+        } else {
+          console.error('Registration failed');
+        }
+      } catch (error) {
+        console.error('Error during registration:', error);
+      } */
+    }; 
+
+    return (
+        <div>
+          <h2>Register</h2>
+          <form onSubmit={handleSubmit}>
+            <div>
+              <label htmlFor="username">Username:</label>
+              <input
+                type="text"
+                id="username"
+                name="username"
+                value={formData.username}
+                onChange={handleChange}
+              />
+            </div>
+            <div>
+              <label htmlFor="email">Email:</label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+              />
+            </div>
+            <div>
+              <label htmlFor="password">Password:</label>
+              <input
+                type="password"
+                id="password"
+                name="password"
+                value={formData.password}
+                onChange={handleChange}
+              />
+            </div>
+            <button type="submit">Register</button>
+          </form>
+        </div>
+      );
+    };
+    
+    export default RegisterPanel;
