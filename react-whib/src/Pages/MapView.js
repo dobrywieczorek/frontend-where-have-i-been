9a2963b45b776eb589f3b19c8dff5cba4398566a
Gelczynski <jakub.gelczynski@studenci.collegiumwitelona.pl>
2023-12-06 22:51:14 +0100

    set displaying different maps for logged & guests

--- react-whib/src/Pages/MapView.js
+++ react-whib/src/Pages/MapView.js
@@ -8,61 +8,68 @@ import shadowUrl from '../../node_modules/leaflet/dist/images/marker-shadow.png'
 
 const MapView = () => {
 	const [mapInitialized, setMapInitialized] = useState(false);
+	const access_token = localStorage.getItem('access_token');
 
 	const getPinsData = async () => {
 		try {
-		const access_token = localStorage.getItem('access_token');
-		
-		const res = await fetch('http://localhost:8000/api/map-pins', {
-			method: 'get',
-			headers: { Authorization: `Bearer ${access_token}` },
-		});
-		const json = await res.json();
-		return json;
-		} catch (err) {
-		console.log(err);
-		return [];
+			const res = await fetch('http://localhost:8000/api/map-pins', {
+				method: 'get',
+				headers: { Authorization: `Bearer ${access_token}` },
+			});
+			const json = await res.json();
+			return json;
+			} catch (err) {
+			console.log(err);
+			return [];
 		}
 	};
 
 	useEffect(() => {
 		const fetchData = async () => {
-		const pinsData = await getPinsData();
-		console.log("pinezki: ", pinsData);
-		let locations = pinsData.map_pins;
+			const pinsData = await getPinsData();
+			console.log("pinezki: ", pinsData);
+			let locations = pinsData.map_pins;
 
-		if (!mapInitialized) {
-			const map = L.map('map').setView([51.505, -0.09], 13);
-			L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-			attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-			}).addTo(map);
+			if (!mapInitialized && access_token) {
+				const map = L.map('map').setView([51.505, -0.09], 13);
+				L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+				attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+				}).addTo(map);
 
-			const customIcon = new L.Icon({
-			iconUrl,
-			iconRetinaUrl,
-			iconSize: [25, 41],
-			iconAnchor: [12, 41],
-			popupAnchor: [1, -34],
-			shadowUrl,
-			shadowSize: [41, 41],
-			});
+				const customIcon = new L.Icon({
+				iconUrl,
+				iconRetinaUrl,
+				iconSize: [25, 41],
+				iconAnchor: [12, 41],
+				popupAnchor: [1, -34],
+				shadowUrl,
+				shadowSize: [41, 41],
+				});
 
-			let favourite;
-			locations.map((location) => {
-				if(location.favourite) {
-					favourite = "tak";
-				}
-				else favourite = "nie";
-			L.marker([location.latitude, location.longitude], { icon: customIcon })
-				.addTo(map)
-				.bindPopup(`Pin name: ${location.pin_name}<br>Ulubiony: ${favourite}
-				<br>Description: ${location.description}<br>Latitude: ${location.latitude}
-				<br>Longitude: ${location.longitude}`)
-				.openPopup();
-			});
+				let favourite;
+				locations.map((location) => {
+					if(location.favourite) {
+						favourite = "tak";
+					}
+					else favourite = "nie";
+				L.marker([location.latitude, location.longitude], { icon: customIcon })
+					.addTo(map)
+					.bindPopup(`Pin name: ${location.pin_name}<br>Ulubiony: ${favourite}
+					<br>Description: ${location.description}<br>Latitude: ${location.latitude}
+					<br>Longitude: ${location.longitude}`)
+					.openPopup();
+				});
 
-			setMapInitialized(true);
-		}
+				setMapInitialized(true);
+			}
+			else if (!mapInitialized && !access_token) {
+				const map = L.map('map').setView([51.505, -0.09], 13);
+				L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+				attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+				}).addTo(map);
+
+				setMapInitialized(true);
+			}
 		};
 
 		fetchData();
