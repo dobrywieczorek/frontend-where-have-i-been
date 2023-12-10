b49c2b3cf07b77ce7d4f046a027433f85c2d4f68
W-Worach <115943399+W-Worach@users.noreply.github.com>
2023-12-10 17:35:58 +0100

    nicer page view and button for route to searched user profile

--- react-whib/src/Pages/UserSearchPanel.js
+++ react-whib/src/Pages/UserSearchPanel.js
@@ -1,10 +1,16 @@
 import React, { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate  } from 'react-router-dom';
 
 export default function UserSearchPanel() {
+    const url = 'http://localhost:8000/api';
     const [searchQuery, setSearchQuery] = useState('');
     const [searchResults, setSearchResults] = useState([]);
     const [error, setError] = useState('');
+    const navigate = useNavigate();
+    function navigateToProfile(userId) {
+        navigate(`/profile/${userId}`);
+    }
+
 
     async function handleSearch(event) {
         event.preventDefault();
@@ -25,7 +31,7 @@ export default function UserSearchPanel() {
                 modifiedSearchQuery = searchQuery.substring(1);
             }
 
-            const response = await fetch(`http://localhost:8000/api/${endpoint}?${searchParam}=${modifiedSearchQuery}`);
+            const response = await fetch(`${url}/${endpoint}?${searchParam}=${modifiedSearchQuery}`);
             const data = await response.json();
     
             if (data && data.users && data.users.success) {
@@ -42,7 +48,7 @@ export default function UserSearchPanel() {
                 } else {
                     // No users found
                     setSearchResults([]);
-                    setError('Brak wyników dla podanej nazwy użytkownika.');
+                    setError('Brak wyników dla szukanego użytkownika.');
                 }
             } else {
                 setSearchResults([]);
@@ -56,31 +62,42 @@ export default function UserSearchPanel() {
     }
 
     return (
-        <div className="mx-auto h-screen grid content-center">
-            <div className="flex flex-col items-center pb-28">
-                <h3 className="text-2xl font-bold mb-2">Wyszukiwanie użytkowników</h3>
-                <form className="w-full sm:w-1/2 md:w-1/2 lg:w-1/3 xl:w-1/3 flex flex-col items-center" onSubmit={handleSearch}>
-                    <div className="mb-3 w-4/6">
-                        <label htmlFor="search" className="block text-gray-700">Nazwa użytkownika</label>
-                        <input
-                            type="text"
-                            className="w-full px-3 py-2 border rounded-md focus:outline-none focus:border-blue-500"
-                            id="search"
-                            value={searchQuery}
-                            onChange={(e) => setSearchQuery(e.target.value)}
-                        />
-                    </div>
-                    <p className="text-red-500 mb-3" id="error">{error}</p>
-                    <button type="submit" className="w-4/6 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                        Szukaj
-                    </button>
-                </form>
+        <div className="container mx-auto mt-10">
+            <div className="max-w-md mx-auto bg-white rounded-lg overflow-hidden shadow-md">
+                <div className="p-6">
+                    <h3 className="text-2xl font-bold mb-4">Wyszukiwanie użytkowników</h3>
+                    <form onSubmit={handleSearch}>
+                        <div className="mb-4">
+                            <label htmlFor="search" className="block text-gray-700 mb-2">Nazwa użytkownika</label>
+                            <input
+                                type="text"
+                                className="w-full px-3 py-2 border rounded-md focus:outline-none focus:border-blue-500"
+                                id="search"
+                                placeholder="Wyszukaj użytkownika..."
+                                value={searchQuery}
+                                onChange={(e) => setSearchQuery(e.target.value)}
+                            />
+                        </div>
+                        <p className="text-red-500 mb-4" id="error">{error}</p>
+                        <button type="submit" className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                            Szukaj
+                        </button>
+                    </form>
+                </div>
                 {searchResults.length > 0 && (
-                    <div className="mt-8 w-4/6">
+                    <div className="p-6 bg-gray-100">
                         <h4 className="text-lg font-bold mb-3">Wyniki wyszukiwania:</h4>
                         <ul>
                             {searchResults.map((user) => (
-                                <li key={user.id}>{user.name} - {user.email}</li> //SEARCHED USERS DATA
+                                <li key={user.id} className="mb-2 border-b pb-2 flex justify-between items-center">
+                                    <span className="">{user.name} #{user.id}</span>
+                                    <button
+                                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-3 rounded focus:outline-none focus:shadow-outline"
+                                        onClick={() => navigateToProfile(user.id)}
+                                    >
+                                        View Profile
+                                    </button>
+                                </li>
                             ))}
                         </ul>
                     </div>
