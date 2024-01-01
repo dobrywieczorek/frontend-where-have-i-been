df3ca2acf21ac6548ece9cb2996971a0168061a9
W-Worach <115943399+W-Worach@users.noreply.github.com>
2024-01-01 16:32:57 +0100

    edit user page

--- react-whib/src/components/EditUser.js
+++ react-whib/src/components/EditUser.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function validatePasswordStrength(password) {
     const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
@@ -12,9 +13,10 @@ function EditUserPage() {
     const [passwordError, setPasswordError] = useState('');
     const [nameError, setNameError] = useState('');
     const [error, setError] = useState('');
+    const navigate = useNavigate();
 
     const url = 'http://localhost:8000/api';
-    const token = "10|NJbXucIu4xcKfHbzopO85kdTRSPwBx8ogDsOQOi8ff801ea1";
+    const token = localStorage.getItem('access_token');
     const requestOptions = {
         method: 'POST',
         headers: {
@@ -26,6 +28,12 @@ function EditUserPage() {
     useEffect(() => {
         const fetchUserData = async () => {
             try {
+                if (!token) {
+                    console.error('Access token not found in localStorage. Redirecting to login page.');
+                    navigate("/login");
+                    return;
+                }
+
                 const response = await fetch(`${url}/whoami`, requestOptions);
                 const data = await response.json();
                 console.log(data);
@@ -39,6 +47,7 @@ function EditUserPage() {
         fetchUserData();
     }, []);
 
+    /*
     const handleSubmit = async (e) => {
         e.preventDefault();
         
@@ -72,14 +81,78 @@ function EditUserPage() {
             setError(error.message);
             console.error('Fetch error:', error);
         }
+    }; */
+
+    const handleUpdateUsername = async () => {
+
+        if (name.trim() === '' || name.includes('#')) {
+            setNameError('Nazwa nie może być pusta i zawierać symbol "#"');
+            return;
+        }
+        setNameError('');
+
+        try {
+            const response = await fetch(`${url}/edituser`, {
+                ...requestOptions,
+                body: JSON.stringify({ name }),
+            });
+
+            if (!response.ok) {
+                throw new Error('Wystąpił błąd podczas aktualizacji nazwy konta.');
+            }
+
+        } catch (error) {
+            setError(error.message);
+            console.error('Fetch error:', error);
+        }
+    };
+
+    const handleUpdatePassword = async () => {
+
+        if (!validatePasswordStrength(password)) {
+            setPasswordError('Hasło powinno zawierać co najmniej 8 znaków, w tym wielkie i małe litery, cyfry i znaki specjalne');
+            return;
+        }
+        setPasswordError('');
+
+        try {
+            const response = await fetch(`${url}/edituser`, {
+                ...requestOptions,
+                body: JSON.stringify({ password }),
+            });
+
+            if (!response.ok) {
+                throw new Error('Wystąpił błąd podczas aktualizacji hasła.');
+            }
+
+        } catch (error) {
+            setError(error.message);
+            console.error('Fetch error:', error);
+        }
+    };
+
+    const handleUpdateDescription = async () => {
+        try {
+            const response = await fetch(`${url}/edituser`, {
+                ...requestOptions,
+                body: JSON.stringify({ description }),
+            });
+
+            if (!response.ok) {
+                throw new Error('Wystąpił błąd podczas aktualizacji opisu.');
+            }
+
+        } catch (error) {
+            setError(error.message);
+            console.error('Fetch error:', error);
+        }
     };
 
     return (
         <div className="flex justify-center items-center h-screen">
             <div className="max-w-md w-full px-6 py-10 bg-white shadow-lg rounded-lg">
                 <h1 className="text-3xl mb-8 font-semibold text-gray-800 text-center">Ustawienia użytkownika</h1>
-                <form onSubmit={handleSubmit}>
-                    <div className="mb-6">
+                <div className="mb-6">
                         <label className="block text-gray-700 text-lg font-semibold mb-3" htmlFor="name">
                             Nowa nazwa konta:
                             <input
@@ -93,6 +166,13 @@ function EditUserPage() {
                         {nameError && (
                             <p className="text-red-500 text-sm italic">{nameError}</p>
                         )}
+                        <button
+                            type="button"
+                            onClick={handleUpdateUsername}
+                            className="bg-green-500 text-white py-3 px-6 rounded-md hover:bg-green-600 focus:outline-none focus:bg-green-600"
+                        >
+                            Aktualizuj nazwę konta
+                        </button>
                     </div>
                     <div className="mb-6">
                         <label className="block text-gray-700 text-lg font-semibold mb-3" htmlFor="password">
@@ -108,6 +188,13 @@ function EditUserPage() {
                         {passwordError && (
                             <p className="text-red-500 text-sm italic">{passwordError}</p>
                         )}
+                        <button
+                            type="button"
+                            onClick={handleUpdatePassword}
+                            className="bg-green-500 text-white py-3 px-6 rounded-md hover:bg-green-600 focus:outline-none focus:bg-green-600"
+                        >
+                            Aktualizuj hasło
+                        </button>
                     </div>
                     <div className="mb-6">
                         <label className="block text-gray-700 text-lg font-semibold mb-3" htmlFor="description">
@@ -119,17 +206,17 @@ function EditUserPage() {
                                 onChange={(e) => setDescription(e.target.value)}
                             />
                         </label>
+                        <button
+                            type="button"
+                            onClick={handleUpdateDescription}
+                            className="bg-green-500 text-white py-3 px-6 rounded-md hover:bg-green-600 focus:outline-none focus:bg-green-600"
+                        >
+                            Aktualizuj opis
+                        </button>
                     </div>
-                    <button
-                        type="submit"
-                        className="bg-blue-500 text-white py-3 px-6 rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
-                    >
-                        Zapisz zmiany
-                    </button>
-                </form>
             </div>
         </div>
-    );         
+    );
 }
 
 export default EditUserPage;
